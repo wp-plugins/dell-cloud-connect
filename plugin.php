5fb3103b9d6211b53a182b3148d6d50c82acce35
clintonb11 <clintonb11@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-11 22:00:52 +0000

    - Added plugin page for cloud computing
    git-svn-id: https://plugins.svn.wordpress.org/dell-cloud-connect/trunk@543128 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: Dell Cloud Connect
+Plugin URI: http://marketing.dell.com/cloudcomputing-widget
+Description: Adds a widget to display the latest news and information from the most influential cloud computing blogs.
+Version: 0.2.0
+Author: Dell
+Author URI: http://www.dell.com
+
+
+Installing
+1. Copy cloud_connect folder to your plugins folder /wp-content/plugins/
+2. Activate it through the plugin management screen.
+3. Go to Appearance->Widgets and drag and drop the widget to wherever you want to show it.
+*/
+
+
+define('DELL_CONNECT_DIVISION', 'cloudcomputing');
+define('DELL_CONNECT_PLUGIN_NAME', 'Dell Cloud Connect');
+define('DELL_CONNECT_SETTINGS_MENU_TITLE', 'Cloud Connect Setting');
+
+define('DELL_CONNECT_PLUGIN_URL',  WP_PLUGIN_URL.'/'.plugin_basename(dirname(__FILE__)));
+define('DELL_CONNECT_PLUGIN_PATH', WP_CONTENT_DIR.'/plugins/'.plugin_basename(dirname(__FILE__)));
+
+require(DELL_CONNECT_PLUGIN_PATH . '/connect.php');
